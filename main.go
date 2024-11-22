b4e4461013805c0f1a89b34273281aaaacab6cd5
slashexx <dhruvpuri.35@gmail.com>
2024-11-23 02:02:19 +0530

    Add a working post function
    Signed-off-by: slashexx <dhruvpuri.35@gmail.com>

--- main.go
+++ main.go
@@ -2,27 +2,62 @@ package main
 
 import (
 	"context"
-	firebase "firebase.google.com/go"
-	"google.golang.org/api/option"
+	// "encoding/json"
+	"fmt"
 	"log"
+
+	"gofr.dev/pkg/gofr"
+
+	"github.com/slashexx/firebasecheck/firebaseSetup"
 )
 
+type sampleData struct {
+	ID      string `json:'id'`
+	Title   string `json:'title'`
+	Content string `jsonL'content'`
+}
+
 func main() {
-	ctx := context.Background()
-	sa := option.WithCredentialsFile("firebaseConfig.json")
+	app := gofr.New()
 
-	app, err := firebase.NewApp(ctx, nil, sa)
+	app.GET("/greet", func(ctx *gofr.Context) (interface{}, error) {
+
+		return "Hello World!", nil
+	})
+
+	app.POST("/posts", addPostHandler)
+
+	app.Run()
+	fmt.Println("Server is running.")
+}
+
+func addPostHandler(ctx *gofr.Context) (interface{}, error) {
+	firebasectx := context.Background()
+
+	fmt.Println("Reached the posting data function")
+
+	client, err := firebaseSetup.InitializeFirebase().Firestore(firebasectx)
 
 	if err != nil {
-		log.Fatalln("Firebase app not initialised")
-	}
-
-	client, err := app.Firestore(ctx)
-
-	if err != nil {
-		log.Fatalln("Firestore cline initialisation failed")
+		log.Fatalln("Error initialising Firestore")
 	}
 
 	defer client.Close()
+
+	var post sampleData
+
+	ctx.Bind(&post)
+
+	if err != nil {
+		log.Fatalln(`Error decoding the post body`)
+	}
+
+	_, err2 := client.Collection("posts").NewDoc().Create(firebasectx, post)
+
+	if err2 != nil {
+		log.Fatalln(`Error creating doc in firestore` + err2.Error())
+	}
+
+	return "Post added", err2
 
 }
